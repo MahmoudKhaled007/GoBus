e6a2978374f6909ddc95cc7f3efda791f4f1edc8
MahmoudKhaled1 <85295806+MahmoudKhaled007@users.noreply.github.com>
2021-09-09 22:09:12 +0200

    with credit and payment models only

--- project/gobus project/models/credit.js
+++ project/gobus project/models/credit.js
@@ -1,7 +1,19 @@
+const payment = require("./payment")
+const p = new payment("1","Credit")
+const Payment_id2=p.id
+
 class credit{
-    constructor(id,CardNumber)
+    
+    constructor(id,CardNumber,CVC,ExpireDate,Payment_id){
+        this.Payment_id=Payment_id2
+        this.CardNumber=CardNumber
+        this.CVC=CVC
+        this.ExpireDate=ExpireDate
+
+    }
 
 
 
 
 }
+module.exports=credit
