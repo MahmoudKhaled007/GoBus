343a750c2d058cc17056a654c92e387e88fca85f
karim tarek <58817281+karimtahmed@users.noreply.github.com>
2021-09-09 21:35:52 +0200

    done bus

--- project/gobus project/routes/bus.js
+++ project/gobus project/routes/bus.js
@@ -1,9 +1,8 @@
 const busRouter = require("express").Router()
 
 const busController = require("../controllers/bus")
-//const middelwares=require("../util/middelwares")
+
 busRouter.get("/",busController.selectBus)
 busRouter.post("/",busController.addBus)
-//busRouter.post("/login",busController.login)
 
 module.exports = busRouter
