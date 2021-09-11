27c16372e039460cfd862bc6e41029173aeef166
karim tarek <58817281+karimtahmed@users.noreply.github.com>
2021-09-11 04:33:36 +0200

    update credit

--- project/gobus project/routes/admin.js
+++ project/gobus project/routes/admin.js
@@ -3,12 +3,12 @@ const ADController = require("../controllers/admin")
 const MiddelWares = require("../util/middelwares")
 
 ADRouter.get("/",ADController.selectADs)
-ADRouter.post("",MiddelWares.checkADAuth,ADController.addAD)
-ADRouter.post("/login",ADController.login)
+ADRouter.post("",ADController.addAD)
+ADRouter.post("/login",MiddelWares.checkADAuth,ADController.login)
 
-ADRouter.put("/:id",ADController.updateAD)
-ADRouter.delete("/:id",ADController.deleteAd)
-ADRouter.patch("/:id",ADController.restoreAD)
+ADRouter.put("/:id",MiddelWares.checkADAuth,ADController.updateAD)
+ADRouter.delete("/:id",MiddelWares.checkADAuth,ADController.deleteAd)
+ADRouter.patch("/:id",MiddelWares.checkADAuth,ADController.restoreAD)
 
 
 
--- project/gobus project/routes/pass.js
+++ project/gobus project/routes/pass.js
@@ -4,10 +4,19 @@ const PasController = require("../controllers/user")
  const MiddelWares = require("../util/middelwares")
 
 PasRouter.get("/",PasController.selectUser)
-PasRouter.post("",MiddelWares.checkADAuth,PasController.addUser)
+
+PasRouter.post("/user",MiddelWares.checkpasAuth,PasController.addUser)
+PasRouter.post("/admin",MiddelWares.checkADAuth,PasController.addUser)
+
 PasRouter.post("/login",PasController.login)
-PasRouter.put("/:id",PasController.updatePas)
-PasRouter.delete("/:id",PasController.deletePas)
-PasRouter.patch("/:id",PasController.restorePas)
+
+PasRouter.put("/user/:id",MiddelWares.checkpasAuth,PasController.updatePas)
+PasRouter.put("/admin/:id",MiddelWares.checkADAuth,PasController.updatePas)
+
+PasRouter.delete("/user/:id",MiddelWares.checkpasAuth,PasController.deletePas)
+PasRouter.delete("/admin/:id",MiddelWares.checkADAuth,PasController.deletePas)
+
+PasRouter.patch("/user/:id",MiddelWares.checkADAuth,PasController.restorePas)
+PasRouter.patch("/admin/:id",MiddelWares.checkADAuth,PasController.restorePas)
 
 module.exports = PasRouter
