fe44a337da7533a75684bce8c0cd86a70a760548
MahmoudKhaled1 <85295806+MahmoudKhaled007@users.noreply.github.com>
2021-09-11 02:49:23 +0200

    .

--- project/gobus project/routes/bus.js
+++
@@ -1,8 +0,0 @@
-const busRouter = require("express").Router()
-
-const busController = require("../controllers/bus")
-
-busRouter.get("/",busController.selectBus)
-busRouter.post("/",busController.addBus)
-
-module.exports = busRouter
--- project/gobus project/routes/trip.js
+++
@@ -1,6 +0,0 @@
-const tripRouter = require("express").Router()
-const tripController= require("../controllers/trip")
-tripRouter.get("/",tripController.selectTrip)
-//tripRouter.post("/",tripController.addBus)
-
-module.exports=tripRouter
