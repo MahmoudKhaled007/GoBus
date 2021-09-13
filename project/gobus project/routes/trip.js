dad160502f7d5832974fc3c7a00d7a296ea1fecf
karim tarek <58817281+karimtahmed@users.noreply.github.com>
2021-09-13 03:28:36 +0200

    before production

--- project/gobus project/routes/trip.js
+++ project/gobus project/routes/trip.js
@@ -1,13 +1,7 @@
 const tripRouter = require("express").Router()
 
 const TripController = require("../controllers/trip")
-//  const MiddelWares = require("../util/middelwares")
 
-// tripRouter.get("/",TripController.TripDetail)
-// tripRouter.post("",TripController.addTrip)
-//tripRouter.put("/:id",TripController.updateTrip)
-//tripRouter.delete("/:id",TripController.deleteTrip)
-//tripRouter.patch("/:id",TripController.restoreTrip)
 
 module.exports = tripRouter
 
